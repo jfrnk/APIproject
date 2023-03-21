da31aa10824b70486f1eee31d6fe22db3b493fa3
Johnathon Franklin <johnathonfranklin@Johnathons-MacBook-Pro-455.local>
2023-03-20 21:32:16 -0500

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
@@ -34,6 +36,16 @@ router.get(
     return res.json(req.user);
   }
 );
+
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
 
