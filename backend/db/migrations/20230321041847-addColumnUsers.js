858ecc0bf2e7ea5eda34b5a3f11017a925af6e49
Johnathon Franklin <johnathonfranklin@Johnathons-MacBook-Pro-455.local>
2023-03-22 13:46:02 -0500

    fix options

--- backend/db/migrations/20230321041847-addColumnUsers.js
+++ backend/db/migrations/20230321041847-addColumnUsers.js
@@ -1,25 +1,21 @@
 
 
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-
+options.tableName = "Users";
 module.exports = {
-  async up (queryInterface, Sequelize) {
-
-     await queryInterface.addColumn(options, 'firstName', {
-      type: Sequelize.STRING
-     });
-     await queryInterface.addColumn(options, 'lastName', {
-      type: Sequelize.STRING
-     });
-
-  },
-
-  async down (queryInterface, Sequelize) {
-    options.tableName = "Users";
-    await queryInterface.removeColumn(options, 'firstName');
-    await queryInterface.removeColumn(options, 'lastName');
-  }
-};
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn(options, 'firstName', {
+      type: Sequelize.STRING(30)
+});
+    await queryInterface.addColumn(options, 'lastName', {
+      type: Sequelize.STRING(30)
+    });
+},
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn(options, 'firstname')
+} };
