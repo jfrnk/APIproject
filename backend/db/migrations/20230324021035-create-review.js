ea550c709a75b0488e3c3f2a7de41f1915b455e0
jfrnk <jrfranklin1@crimson.ua.edu>
2023-03-28 10:43:09 -0500

    refer to last

--- backend/db/migrations/20230324021035-create-review.js
+++ backend/db/migrations/20230324021035-create-review.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
-options.tableName = "Bookings";
+options.tableName = "Reviews";
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Reviews', {
