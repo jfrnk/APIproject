3bee2a85b506da1429891c71389f941fbce78ffa
jfrnk <jrfranklin1@crimson.ua.edu>
2023-04-25 13:27:56 -0500

    delete spots

--- backend/db/seeders/20230323151330-spots.js
+++ backend/db/seeders/20230323151330-spots.js
@@ -10,42 +10,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     options.tableName = 'Spots';
     return queryInterface.bulkInsert(options, [
-      {
-        address: '123 grove st',
-        city: 'fakecity',
-        state: 'MA',
-        country: 'United States',
-        lat: 83.96,
-        lng: -100.9,
-        name: 'Container House',
-        description: 'House made from containers',
-        price: 200.98,
-        ownerId: 4
-      },
-      {
-        address: '234 user st.',
-        city: 'maybury',
-        state: 'NM',
-        country: 'United States',
-        lat: 76.09,
-        lng: 182.90,
-        name: 'Emirates',
-        description: 'THe emirates',
-        price: 340.90,
-        ownerId: 4
-      },
-      {
-        address: '567 franklin st',
-        city: 'askda',
-        state: 'Dubai',
-        country: 'Dubai',
-        lat: 180.98,
-        lng: 76.95,
-        name: 'United',
-        description: 'United hotel group',
-        price: 394.98,
-        ownerId: 4
-      }
+
     ], {});
   },
 
